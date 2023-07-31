57aed92092e903df34e9a1364027de3491f8538b
Anmol Shrestha <nmol.crestha@gmail.com>
2023-07-31 06:43:12 +0545

    useEffect use

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,135 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+const KEY = "31948a14";
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [query, setQuery] = useState("");
+
+  const inputQuery = "avenger";
+  useEffect(function () {
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        setError('');
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+        );
+
+        if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+        const data = await res.json();
+
+        console.log(data.Response);
+        if (data.Response === "False") throw new Error("Movie not found");
+        setMovies(data.Search);
+        setError("");
+      } catch (err) {
+        setError(err.message);
+      }finally{
+        setIsLoading(false);
+      }
+    }
+
+    if(query.length < 3){
+      setMovies([]);
+      setError('');
+      return;
+    }
+    fetchMovies();
+  }, [query]);
+  console.log(movies);
   return (
     <>
-    <Navgation >
-      <Search/>
-      <NumResults movies={movies}/>
-    </Navgation>
-    <Main>
-      <Box>
-        <MovieList movies={movies}/>
-      </Box>
-      <Box>
-        <WatchedSummery watched={watched}/>
-        <WatchedMovieList watched={watched}/>
-      </Box>
-      {/* <WactchedBox/>  */}
-    </Main>
+      <Navgation>
+        <Search query={query} setQuery={setQuery} />
+        <NumResults movies={movies} />
+      </Navgation>
+      <Main>
+        <Box>
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {isLoading && <Loader/>}
+          {!isLoading && !error && <MovieList movies={movies}/>}
+          {error && <ErrorMessage message={error}/>}
+          </Box>
+        <Box>
+          <WatchedSummery watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
+        {/* <WactchedBox/>  */}
+      </Main>
     </>
   );
+}
+
+const Loader = () => {
+  return <p className="loader">Loading...</p>;
+};
+
+const ErrorMessage = ({message}) =>{
+  return (<p className="error">{message}</p>);
 }
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-  const Navgation = ({children}) => {
-    
-    return(
-      <nav className="nav-bar">
-        <Logo/>
-        {children}
+const Navgation = ({ children }) => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
     </nav>
-    );
-  }
+  );
+};
 
-  const Logo = () => {
-    return(
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-    )
-    
-  }
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+};
 
-  const NumResults = ({movies}) => {
-    return(
-      <p className="num-results">
-        Found <strong>{movies.length}</strong> results
-      </p>
-    )
-  }
+const NumResults = ({ movies }) => {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+};
 
-  const Search = () => {
-    const [query, setQuery] = useState("");
-
-    return(
-      <input
+const Search = ({query,setQuery}) => {
+  return (
+    <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
     />
-    );
-  }
+  );
+};
 
-  const MovieList = ({movies}) => {
-    return(
-      <ul className="list">
-      {movies?.map((movie) => <Movie movie={movie} key={movie.imdbID}/>)}
+const MovieList = ({ movies }) => {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
     </ul>
-    )
-  }
+  );
+};
 
-  const Movie = ({movie}) => {
-    return (
-      <li>
+const Movie = ({ movie }) => {
+  return (
+    <li>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
@@ -136,58 +139,57 @@ const average = (arr) =>
         </p>
       </div>
     </li>
-    )
-  }
+  );
+};
 
-  const Box = ({children}) => {
-    const [isOpen, setIsOpen] = useState(true);
+const Box = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
 
-    return(
-      <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen((open) => !open)}
-      >
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
       {isOpen && children}
     </div>
-    );
-  }
+  );
+};
 
-  // const WactchedBox = () => {
-  //   const [watched, setWatched] = useState(tempWatchedData);
-  //   const [isOpen2, setIsOpen2] = useState(true);
+// const WactchedBox = () => {
+//   const [watched, setWatched] = useState(tempWatchedData);
+//   const [isOpen2, setIsOpen2] = useState(true);
 
-  //   return(
-  //     <div className="box">
-  //     <button
-  //       className="btn-toggle"
-  //       onClick={() => setIsOpen2((open) => !open)}
-  //     >
-  //       {isOpen2 ? "–" : "+"}
-  //     </button>
-  //     {isOpen2 && (
-  //       <>
-  //         <WatchedSummery watched={watched}/>
-  //         <WatchedMovieList watched={watched}/>
-  //       </>
-  //     )}
-  //   </div>
-  //   )
-  // }
+//   return(
+//     <div className="box">
+//     <button
+//       className="btn-toggle"
+//       onClick={() => setIsOpen2((open) => !open)}
+//     >
+//       {isOpen2 ? "–" : "+"}
+//     </button>
+//     {isOpen2 && (
+//       <>
+//         <WatchedSummery watched={watched}/>
+//         <WatchedMovieList watched={watched}/>
+//       </>
+//     )}
+//   </div>
+//   )
+// }
 
-  const WatchedMovieList = ({watched}) => {
-    return(
-      <ul className="list">
-      {watched.map((movie) => <WatchedMovie key={movie.imdbID} movie={movie}/>)}
+const WatchedMovieList = ({ watched }) => {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie key={movie.imdbID} movie={movie} />
+      ))}
     </ul>
-    )
-  }
+  );
+};
 
-  const WatchedMovie = ({movie}) => {
-    return(
-      <li key={movie.imdbID}>
+const WatchedMovie = ({ movie }) => {
+  return (
+    <li key={movie.imdbID}>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
@@ -205,44 +207,39 @@ const average = (arr) =>
         </p>
       </div>
     </li>
-    )
-  }
+  );
+};
 
-  const WatchedSummery = ({watched}) => {
-    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-    const avgUserRating = average(watched.map((movie) => movie.userRating));
-    const avgRuntime = average(watched.map((movie) => movie.runtime));
+const WatchedSummery = ({ watched }) => {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
 
-    return (
-      <div className="summary">
-            <h2>Movies you watched</h2>
-            <div>
-              <p>
-                <span>#️⃣</span>
-                <span>{watched.length} movies</span>
-              </p>
-              <p>
-                <span>⭐️</span>
-                <span>{avgImdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{avgUserRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{avgRuntime} min</span>
-              </p>
-            </div>
-          </div>
-    );
-  }
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-  const Main = ({children}) => {
-
-    return (
-      <main className="main">
-        {children}
-    </main>
-    )
-  }
+const Main = ({ children }) => {
+  return <main className="main">{children}</main>;
+};
