5978a1c5cde3471ec71a4e9fc0f29ba2738b212f
Anmol Shrestha <nmol.crestha@gmail.com>
2023-08-09 06:52:24 +0545

    dynamic title with cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -297,6 +297,16 @@ const SelectedMovie = ({ selectedId, onClose, onAddWatched, watched }) => {
     },
     [selectedId]
   );
+
+  useEffect(function(){
+    if(!title) return;
+    document.title = `Movie | ${title}`;
+    
+    return function(){
+      document.title = "usePopcorn";
+    }
+  },[title]);
+
   return (
     <div className="details">
       {isLoading ? (
